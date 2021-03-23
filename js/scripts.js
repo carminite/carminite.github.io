0db2da3579d830e48fc50b8120c4c62cf71cd1e2
carminite <62965590+carminite@users.noreply.github.com>
2021-03-22 20:56:13 -0400

    jesus christ i did nothing
    no

--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,7 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  $('#portfolio').magnificPopup({
+  $('#tutorial').magnificPopup({
     delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
